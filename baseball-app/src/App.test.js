fa5f47b82d7c4495205fe32089be9495e3df0bd2
Tyler Gilchrist <tyler668@gmail.com>
2019-09-05 18:35:52 -0400

    adding jest and rtl

--- baseball-app/src/App.test.js
+++ baseball-app/src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {increment, reset} from './App';
+import 'jest-dom/extend-expect';
+import 'react-testing-library/cleanup-after-each';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test("the sum function sums two integers", () => {
+  expect(increment(3)).toBe(4);
+});
+
